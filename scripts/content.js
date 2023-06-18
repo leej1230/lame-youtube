ed3799ed13ee39c81d5d3a571d1994fcc819df17
leej1230 <leejaewoo1230@gmail.com>
2023-06-18 02:45:39 -0700

    completely works on youtube home

--- scripts/content.js
+++ scripts/content.js
@@ -1,7 +1,27 @@
 const thumbnails = document.querySelectorAll("ytd-thumbnail");
 
-if (thumbnails) {
-    Array.prototype.forEach.call(thumbnails, (child) => {
-        child.parentNode.removeChild(child)
-    })
+function removeThumbnail() {
+    const thumbnails = document.querySelectorAll("ytd-thumbnail");
+
+    if (thumbnails) {
+        Array.prototype.forEach.call(thumbnails, (child) => {
+            child.parentNode.removeChild(child)
+        })
+    }
 }
+
+var observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+        if (mutation.addedNodes.length) {
+            // console.log("list updated");
+            removeThumbnail();
+        }
+    })
+})
+
+removeThumbnail();
+window.scrollTo(0, 0);
+const yt = document.querySelector("#contents");
+observer.observe(yt, {
+    childList: true
+});
